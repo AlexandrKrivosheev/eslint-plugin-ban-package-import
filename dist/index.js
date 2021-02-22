fed17c9613e5bf50555dce74edd997a8c21ce358
Alexandr Krivosheev <alexandrkrivosheevmail@gmail.com>
2021-02-22 21:12:19 +0100

    feat: rm dist from ignore

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.banPackageImport = void 0;
+var banPackageImport_1 = require("./banPackageImport");
+Object.defineProperty(exports, "banPackageImport", { enumerable: true, get: function () { return banPackageImport_1.banPackageImport; } });
