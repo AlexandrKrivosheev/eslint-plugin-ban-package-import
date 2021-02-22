9576ee801de68400f42a1b564710cda0e048159d
Alexandr Krivosheev <alexandrkrivosheevmail@gmail.com>
2021-02-22 21:14:45 +0100

    feat: build

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.banPackageImport = void 0;
+exports.rules = void 0;
 var banPackageImport_1 = require("./banPackageImport");
-Object.defineProperty(exports, "banPackageImport", { enumerable: true, get: function () { return banPackageImport_1.banPackageImport; } });
+exports.rules = {
+    "ban-package-import": banPackageImport_1.banPackageImport,
+};
